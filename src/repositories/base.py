84a728e79e9edcc87725ecbc65ecca7865c821e1
khkim <kkh147.17.3@gmail.com>
2024-03-21 18:28:08 +0900

    fix

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -14,10 +14,23 @@ class BaseRepository(Generic[T]):
     def __init__(self, db: Annotated[Session, Depends(get_db)]):
         self.db = db
 
-    def get_by_id(self, entity_id: int) -> T:
-        return self.db.query(T).filter(T.id == entity_id).first()
+    def find_by_id(self, entity_id: int) -> T:
+        return self.find_by(id=entity_id).pop()
 
     def delete_by_id(self, entity_id: int) -> None:
         entity = self.db.query(T).filter(T.id == entity_id).first()
         if entity is not None:
             self.db.delete(entity)
+
+    def find_by(self, **kwargs) -> list[T]:
+
+        query = self.db.query(T)
+        for k, v in kwargs.items():
+            if hasattr(T, k):
+                query = query.filter(T.k == v)
+            else:
+                raise ValueError(f"field name {k} is not a valid for entity the {T.__class__.__name__}")
+
+
+
+        return query.all()
--- tests/test_product.py
+++ tests/test_product.py
@@ -2,7 +2,7 @@ import pytest
 from .test_config import client
 
 
-def test_새로운_유저_생성():
-    res = client.get_by_id("/user/3")
+def test_endpoint():
+    res = client.find_by_id("/user/3")
 
     assert res.status_code == 200
