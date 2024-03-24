2d22d53f2472151b75c71af3d8fd23756dce0cea
kkh <kkh147.17.3@gmail.com>
2024-03-24 15:50:07 +0900

    temp

--- src/database.py
+++ src/database.py
@@ -11,6 +11,7 @@ user = os.getenv("DB_USER")
 passwd = os.getenv("DB_PASSWORD")
 url = os.getenv("DB_URL")
 db_database = os.getenv("DB_DATABASE")
+print(user, passwd, url, db_database)
 engine = create_engine(f"postgresql://{user}:{passwd}@{url}:5432/{db_database}", echo=True)
 session_local = scoped_session(sessionmaker(autocommit=False, bind=engine))
 
