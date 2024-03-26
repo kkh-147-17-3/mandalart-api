dd86c06cd75908e9c3ab20dce64946a8d922dabd
kkh147.17.3 <kkh147.17.3@gmail.com>
2024-03-26 22:38:26 +0900

    fix: remove commented codes

--- src/main.py
+++ src/main.py
@@ -40,6 +40,3 @@ app.add_middleware(CORSMiddleware,
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"], )
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
