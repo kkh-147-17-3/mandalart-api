bb875ea7417ed83895f1c6bfa6b30f5101ae9d8b
khkim <kkh147.17.3@gmail.com>
2024-03-27 13:31:05 +0900

    feat: add __init__ in src/views dir

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -2,3 +2,4 @@ from .user import router as user_router
 from .token import router as token_router
 from .sheet import router as sheet_router
 from .cell import router as cell_router
+from .healthcheck import router as healthcheck_router
