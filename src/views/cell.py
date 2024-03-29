1d5060a4c375e568602495d7f7a86ea8ed734f8f
khkim <kkh147.17.3@gmail.com>
2024-03-29 09:40:04 +0900

    feat: add getting cells route

--- src/views/cell.py
+++ src/views/cell.py
@@ -1,8 +1,10 @@
-from fastapi import Depends
+from typing import Annotated
+
+from fastapi import Depends, Query
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
 
-from models.cell import CreateCellDto, GetCellDto, UpdateCellDto
+from models.cell import GetCellDto, UpdateCellDto, GetCellWithChildrenDto
 from models.response import GenericResponse
 from services.cell import CellService
 
@@ -13,26 +15,28 @@ router = InferringRouter()
 class CellView:
     cell_service: CellService = Depends(CellService)
 
-    @router.post("/cell", response_model=GenericResponse[GetCellDto],
-                 summary="만다르트의 한 개의 셀을 생성합니다.",
-                 tags=["cell"])
-    def create_cell(self, dto: CreateCellDto):
-        """
-        depth: 1(정중앙), 2(정중앙에서 주위 8 개),3(depth=2 8개 셀 주변에 있는 8개의 셀)\n
-        order \n
-        |1|2|3|\n
-        |4|X|5|\n
-        |6|7|8|\n
-        * depth: 1인 경우, order 는 무조건 1 \n
-        color: hex code (ex: #fff, #FFF, #ffffff 모두 가능)
-        """
-        user_id = 1
-        result = self.cell_service.create_cell(dto, user_id)
-        return GenericResponse(status=200, data=result, message="Success")
-
     @router.put("/cell/{cell_id}", response_model=GenericResponse[GetCellDto],
                 summary="만다르트 셀 한 개의 내용을 수정합니다.", tags=["cell"])
     def update_cell(self, cell_id: int, dto: UpdateCellDto):
         user_id = 1
         result = self.cell_service.update_cell(dto, user_id, cell_id)
         return GenericResponse(status=200, data=result, message="Success")
+
+    @router.get("/cell/{cell_id}", response_model=GenericResponse[GetCellWithChildrenDto],
+                summary="만다르트 셀 한 개의 정보를 가져옵니다.", tags=["cell"])
+    def get_cell(self, cell_id: int):
+        user_id = 1
+        result = self.cell_service.get_by_id(user_id, cell_id)
+        return GenericResponse(status=200, data=result, message="Success")
+
+    @router.get("/sheet/{sheet_id}/cell", response_model=GenericResponse[list[GetCellDto]],
+                summary="가장 중앙의 만다르트 셀 정보를 포함한 만다르트 시트 정보를 불러옵니다.", tags=["sheet"])
+    def get_sheet_cells(self, sheet_id: int,
+                        depth: Annotated[int, Query(ge=1, le=2)],
+                        parent_order: Annotated[int, Query(ge=0, le=7)] = 0):
+        """
+        return 중 `step_1_cell.children`은 step_1_cell 필드 내용과 동일
+        """
+        user_id = 1
+        result = self.cell_service.get_by_sheet_id_and_depth_and_parent_order(user_id, sheet_id, depth, parent_order)
+        return GenericResponse(status=200, data=result, message="Successfully fetched")
