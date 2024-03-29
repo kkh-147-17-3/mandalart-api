12647b5a8d1375a24949171b75d45867e570ee04
khkim <kkh147.17.3@gmail.com>
2024-03-29 09:38:10 +0900

    feat: add base repository method create, update, and delete

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -18,13 +18,20 @@ class BaseRepository(Generic[T]):
         self.db = db
         self.model = (get_args(self.__orig_bases__[0])[0])  # type: ignore
 
-    def find_by_id(self, entity_id: int) -> T:
-        return self.find_by(id=entity_id).pop()
+    def find_by_id(self, entity_id: int) -> T | None:
+        return self.db.query(self.model).filter(self.model.id == entity_id).first()
+
+    def create_or_update(self, entity: T) -> T:
+        self.db.add(entity)
+        return entity
+
+    def delete(self, entity: T) -> None:
+        self.db.delete(entity)
 
     def delete_by_id(self, entity_id: int) -> None:
         entity = self.db.query().filter(self.model.id == entity_id).first()
         if entity is not None:
-            self.db.delete(entity)
+            self.delete(entity)
 
     def validate_kwargs(self, **kwargs):
         for k, v in kwargs.items():
