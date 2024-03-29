15e4630a3d791a5a80aa5c25e0149f6079f736d4
khkim <kkh147.17.3@gmail.com>
2024-03-29 09:40:17 +0900

    feat: validate color

--- src/models/cell.py
+++ src/models/cell.py
@@ -4,16 +4,25 @@ from typing import List, Self, Optional
 from pydantic import BaseModel, Field, validator
 
 
+def validate_color(cls, color: str) -> str:
+    p = re.compile('([0-9a-fA-F]{2}){4}')
+
+    if not p.match(color):
+        raise ValueError(f'Invalid color {color}')
+
+    return color.upper()
+
+
 class BaseCellDto(BaseModel):
     """
-    depth: 1(정중앙), 2(정중앙에서 주위 8 개),3(depth=2 8개 셀 주변에 있는 8개의 셀)\n
+    step: 1(정중앙), 2(정중앙에서 주위 8 개),3(step=2 8개 셀 주변에 있는 8개의 셀)\n
     order \n
     |1|2|3|\n
     |4|X|5|\n
     |6|7|8|\n
     """
-    depth: int = Field(None, ge=1, le=3)
-    order: int = Field(None, ge=1, le=8)
+    step: int = Field(None, ge=1, le=3)
+    order: int = Field(None, ge=0, le=7)
 
 
 class CreateCellDto(BaseCellDto):
@@ -21,22 +30,16 @@ class CreateCellDto(BaseCellDto):
     goal: str
     color: str
     parent_order: int | None = None
-
-    @validator('color')
-    def validate_color(cls, color):
-        p = re.compile('#(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})')
-
-        if not p.match(color):
-            raise ValueError(f'Invalid color {color}')
-
-        return color
+    _validate_color = validator('color', allow_reuse=True)(validate_color)
 
 
 class GetCellDto(BaseCellDto):
     id: int
-    color: str
-    goal: str
-    parent: Optional["GetCellDto"]
+    color: str | None
+    goal: str | None
+    is_completed: bool
+
+    # parent: Optional["GetCellDto"]
 
     class Config:
         orm_mode = True
@@ -44,23 +47,17 @@ class GetCellDto(BaseCellDto):
 
 class GetCellWithChildrenDto(BaseCellDto):
     id: int
-    color: str
-    goal: str
-    children: list["GetCellWithChildrenDto"]
+    color: str | None
+    goal: str | None
+    is_completed: bool
+    children: list[GetCellDto]
 
     class Config:
         orm_mode = True
 
 
 class UpdateCellDto(BaseModel):
-    goal: str
-    color: str
-
-    @validator('color')
-    def validate_color(cls, color):
-        p = re.compile('#(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})')
-
-        if not p.match(color):
-            raise ValueError(f'Invalid color {color}')
-
-        return color
+    goal: str | None = None
+    color: str | None = None  # FFFFFFFF
+    is_completed: bool | None = None
+    _validate_color = validator('color', allow_reuse=True)(validate_color)
