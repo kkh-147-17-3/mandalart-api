d1821ea33b10ee856ba2fc149c5b9d81a9bca17a
khkim <kkh147.17.3@gmail.com>
2024-04-09 18:24:11 +0900

    feat: add repository method

--- src/repositories/user.py
+++ src/repositories/user.py
@@ -4,4 +4,7 @@ from schemas.user import User
 
 
 class UserRepository(BaseRepository[User]):
-    pass
+    def find_by_social_provider_and_social_id(self, social_id: str,
+                                              social_provider: SocialProvider) -> User | None:
+        return self._db.query(User).filter(User.social_id == social_id).filter(
+            User.social_provider == social_provider).order_by(User.id.desc()).first()
