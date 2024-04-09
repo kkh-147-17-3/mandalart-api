7dc127679349e671325d25071862c030d49d11ca
khkim <kkh147.17.3@gmail.com>
2024-04-09 18:24:35 +0900

    feat: add token related methods

--- src/views/token.py
+++ src/views/token.py
@@ -6,16 +6,21 @@ from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
 from starlette.requests import Request
 
-from services.token import TokenService
-from oauth import on_auth
+from models.response import GenericResponse
+from models.token import BaseTokenDto
+from services.token import auth_refresh_wrapper
+from services.login import get_token
 
 router = InferringRouter()
 
 
 @cbv(router)
 class TokenView:
-    token_service: TokenService = Depends(TokenService)
 
-    # @router.get("/token")
-    def get_token(self, req: Request) -> dict:
-        return self.token_service.get_token(req.user)
+    @router.get("/token", tags=["login"],
+                summary="refresh token을 사용하여 새로운 access token과 refresh token을 발급받습니다.",
+                description="Authorization 헤더에 access token 대신 refresh token을 사용하여 새로운 token을 발급받습니다."
+                )
+    def get_token(self, user_id: int = Depends(auth_refresh_wrapper)) -> GenericResponse[BaseTokenDto]:
+        token_dto = get_token(user_id)
+        return GenericResponse(status=200, data=token_dto, message="OK")
