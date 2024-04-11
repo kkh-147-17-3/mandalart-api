8ff1ef52c25ad1483859c76d91818e0c7b570892
khkim <kkh147.17.3@gmail.com>
2024-04-11 10:54:52 +0900

    refactor: import jwt secret from env

--- src/services/token.py
+++ src/services/token.py
@@ -1,14 +1,22 @@
+import os
 from datetime import timedelta, datetime, timezone
 from typing import Dict, Any
 
+from dotenv import load_dotenv
 from fastapi import HTTPException, Depends
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from jose import jwt
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from errors.exceptions import MissingEnvVarException
+
+load_dotenv('src/../.env')
+
+SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+ALGORITHM = os.getenv("JWT_ALGORITHM")
 security = HTTPBearer()
+
+if not SECRET_KEY or not ALGORITHM:
+    raise MissingEnvVarException("JWT_SECRET_KEY and JWT_ALGORITHM are required")
 
 
 def create_access_token(user_id: int, expires_delta: timedelta | None = None) -> str:
