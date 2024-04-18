79fcd11963291ab9867a8e0fa7987eca23a7ae97
khkim <kkh147.17.3@gmail.com>
2024-04-18 11:27:42 +0900

    feat: add apple login test

--- src/views/healthcheck.py
+++ src/views/healthcheck.py
@@ -2,8 +2,12 @@ import os
 from typing import Dict
 
 from fastapi import APIRouter
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
 
 router = APIRouter()
+
+templates = Jinja2Templates(directory='src/templates')
 
 
 @router.get("/")
@@ -12,3 +16,8 @@ async def healthcheck() -> Dict[str, str | None]:
     return {
         "flag": flag
     }
+
+
+@router.get("/login-page")
+def get_login_page(request: Request):
+    return templates.TemplateResponse(name="login.html", context={"request": request})
